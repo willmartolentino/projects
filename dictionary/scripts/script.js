60e0166088463f11ac6332a7923badb24e623a3f
Willmar Tolentino <willmar.tolentino@ninthdesign.com>
2015-02-23 08:27:58 +0800

    modified script.js

--- dictionary/scripts/script.js
+++ dictionary/scripts/script.js
@@ -1,125 +1,125 @@
 (function() {
     var entries = [
         {
-            term: "head",
+            term: 'head',
             ilonggoTerm: '"Ulo"',
-            desc: "The upper part of the human body, or the front or upper part of the body of an animal, typically separated from the rest of the body by a neck, and containing the brain, mouth, and sense organs."
+            desc: 'The upper part of the human body, or the front or upper part of the body of an animal, typically separated from the rest of the body by a neck, and containing the brain, mouth, and sense organs.'
         }, {
-            term: "arms",
+            term: 'arms',
             ilonggoTerm: '"Kamot"',
-            desc: "Each of the two upper limbs of the human body from the shoulder to the hand."
+            desc: 'Each of the two upper limbs of the human body from the shoulder to the hand.'
         }, {
-            term: "body",
+            term: 'body',
             ilonggoTerm: '"Lawas"',
-            desc: "The physical structure, including the bones, flesh, and organs, of a person or an animal."
+            desc: 'The physical structure, including the bones, flesh, and organs, of a person or an animal.'
         }, {
-            term: "legs",
+            term: 'legs',
             ilonggoTerm: '"Paa"',
-            desc: "Each of the limbs on which a person or animal walks and stands."
+            desc: 'Each of the limbs on which a person or animal walks and stands.'
         }, {
-            term: "feet",
+            term: 'feet',
             ilonggoTerm: '"Tiil"',
-            desc: "The lower extremity of the leg below the ankle, on which a person stands or walks."
+            desc: 'The lower extremity of the leg below the ankle, on which a person stands or walks.'
         }, {
-            term: "dog",
+            term: 'dog',
             ilonggoTerm: '"Ido"',
-            desc: "A domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, non-retractile claws, and a barking, howling, or whining voice."
+            desc: 'A domesticated carnivorous mammal that typically has a long snout, an acute sense of smell, non-retractile claws, and a barking, howling, or whining voice.'
         }, {
-            term: "cat",
+            term: 'cat',
             ilonggoTerm: '"Kuring"',
-            desc: "A small domesticated carnivorous mammal with soft fur, a short snout, and retractile claws."
+            desc: 'A small domesticated carnivorous mammal with soft fur, a short snout, and retractile claws.'
         }, {
-            term: "mouse",
+            term: 'mouse',
             ilonggoTerm: '"Ilaga"',
-            desc: "a small rodent that typically has a pointed snout, relatively large ears and eyes, and a long tail."
+            desc: 'a small rodent that typically has a pointed snout, relatively large ears and eyes, and a long tail.'
         }, {
-            term: "lizard",
+            term: 'lizard',
             ilonggoTerm: '"Tiki"',
-            desc: "a reptile that typically has a long body and tail, four legs, movable eyelids, and a rough, scaly, or spiny skin."
+            desc: 'a reptile that typically has a long body and tail, four legs, movable eyelids, and a rough, scaly, or spiny skin.'
         }, {
-            term: "snake",
+            term: 'snake',
             ilonggoTerm: '"Man-og"',
-            desc: "A long limbless reptile which has no eyelids, a short tail, and jaws that are capable of considerable extension."
+            desc: 'A long limbless reptile which has no eyelids, a short tail, and jaws that are capable of considerable extension.'
         }, {
-            term: "happy",
+            term: 'happy',
             ilonggoTerm: '"Malipayon"',
-            desc: "Feeling or showing pleasure or contentment."
+            desc: 'Feeling or showing pleasure or contentment.'
         }, {
-            term: "sad",
+            term: 'sad',
             ilonggoTerm: '"Kasubo"',
-            desc: "Feeling or showing sorrow; unhappy."
+            desc: 'Feeling or showing sorrow; unhappy.'
         }, {
-            term: "angry",
+            term: 'angry',
             ilonggoTerm: '"Akig"',
-            desc: "Feeling or showing strong annoyance, displeasure, or hostility; full of anger."
+            desc: 'Feeling or showing strong annoyance, displeasure, or hostility; full of anger.'
         }, {
-            term: "lonely",
+            term: 'lonely',
             ilonggoTerm: '"Gaisahanon"',
-            desc: "Sad because one has no friends or company."
+            desc: 'Sad because one has no friends or company.'
         }, {
-            term: "scared",
+            term: 'scared',
             ilonggoTerm: '"Kulba-an"',
-            desc: "Fearful; frightened."
+            desc: 'Fearful; frightened.'
         }, {
-            term: "plant",
+            term: 'plant',
             ilonggoTerm: '"Tanom"',
-            desc: "A living organism of the kind exemplified by trees, shrubs, herbs, grasses, ferns, and mosses."
+            desc: 'A living organism of the kind exemplified by trees, shrubs, herbs, grasses, ferns, and mosses.'
         }, {
-            term: "river",
+            term: 'river',
             ilonggoTerm: '"Suba"',
-            desc: "A large natural stream of water flowing in a channel to the sea, a lake, or another river."
+            desc: 'A large natural stream of water flowing in a channel to the sea, a lake, or another river.'
         }, {
-            term: "bird",
+            term: 'bird',
             ilonggoTerm: '"Pispis"',
-            desc: "A warm-blooded egg-laying vertebrate animal distinguished by the possession of feathers, wings, a beak, and typically by being able to fly."
+            desc: 'A warm-blooded egg-laying vertebrate animal distinguished by the possession of feathers, wings, a beak, and typically by being able to fly.'
         }, {
-            term: "bat",
+            term: 'bat',
             ilonggoTerm: '"Kulunatnit"',
-            desc: "A mainly nocturnal mammal capable of sustained flight, with membranous wings that extend between the fingers and limbs."
+            desc: 'A mainly nocturnal mammal capable of sustained flight, with membranous wings that extend between the fingers and limbs.'
         }, {
-            term: "ant",
+            term: 'ant',
             ilonggoTerm: '"Subay"',
-            desc: "A small insect typically having a sting and living in a complex social colony with one or more breeding queens."
+            desc: 'A small insect typically having a sting and living in a complex social colony with one or more breeding queens.'
         }, {
-            term: "food",
+            term: 'food',
             ilonggoTerm: '"Pagkaon"',
-            desc: "Any nutritious substance that people or animals eat or drink or that plants absorb in order to maintain life and growth."
+            desc: 'Any nutritious substance that people or animals eat or drink or that plants absorb in order to maintain life and growth.'
         }, {
-            term: "chair",
+            term: 'chair',
             ilonggoTerm: '"Pulungkuan"',
-            desc: "A separate seat for one person, typically with a back and four legs."
+            desc: 'A separate seat for one person, typically with a back and four legs.'
         }, {
-            term: "bed",
+            term: 'bed',
             ilonggoTerm: '"Hiligdaan"',
-            desc: "A piece of furniture for sleep or rest, typically a framework with a mattress."
+            desc: 'A piece of furniture for sleep or rest, typically a framework with a mattress.'
         }, {
-            term: "rest",
+            term: 'rest',
             ilonggoTerm: '"Pahuway"',
-            desc: "Cease work or movement in order to relax, sleep, or recover strength."
+            desc: 'Cease work or movement in order to relax, sleep, or recover strength.'
         }, {
-            term: "walk",
+            term: 'walk',
             ilonggoTerm: '"Lakat"',
-            desc: "Move at a regular pace by lifting and setting down each foot in turn, never having both feet off the ground at once."
+            desc: 'Move at a regular pace by lifting and setting down each foot in turn, never having both feet off the ground at once.'
         }, {
-            term: "run",
+            term: 'run',
             ilonggoTerm: '"Dalagan"',
-            desc: "Move at a speed faster than a walk, never having both or all the feet on the ground at the same time."
+            desc: 'Move at a speed faster than a walk, never having both or all the feet on the ground at the same time.'
         }, {
-            term: "fast",
+            term: 'fast',
             ilonggoTerm: '"Madasig"',
-            desc: "Moving or capable of moving at high speed."
+            desc: 'Moving or capable of moving at high speed.'
         }, {
-            term: "slow",
+            term: 'slow',
             ilonggoTerm: '"Mahinay"',
-            desc: "Moving or operating, or designed to do so, only at a low speed; not quick or fast."
+            desc: 'Moving or operating, or designed to do so, only at a low speed; not quick or fast.'
         }, {
-            term: "jump",
+            term: 'jump',
             ilonggoTerm: '"Lumpat"',
-            desc: "Push oneself off a surface and into the air by using the muscles in one's legs and feet."
+            desc: 'Push oneself off a surface and into the air by using the muscles in one\'s legs and feet.'
         }, {
-            term: "eat",
+            term: 'eat',
             ilonggoTerm: '"Kaon"',
-            desc: "Put (food) into the mouth and chew and swallow it."
+            desc: 'Put (food) into the mouth and chew and swallow it.'
         }
     ];
 
