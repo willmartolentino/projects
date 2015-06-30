a82d08c750822847eb50ed8d46336fc9192af4de
Willmar Tolentino <willmar.tolentino@ninthdesign.com>
2015-06-30 13:51:49 +0800

    add scripts

--- dragula/scripts/script.js
+++ dragula/scripts/script.js
@@ -1,3 +1,14 @@
 (function() {
-    
+    var doc = document,
+        canvas = doc.getElementById( 'canvas' );
+
+    // Just to check if the <canvas> tag is supported on the user's browser
+    if ( canvas.getContext ) {
+        var ctx = canvas.getContext( '2d' );
+
+        ctx.fillStyle = 'rgba(200, 0, 0,)';
+        ctx.fillRect( 10, 10, 55, 50 )
+    } else {
+        alert( '<canvas> tag is not supported on your browser' );
+    }
 })();
